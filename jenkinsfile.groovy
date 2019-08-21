3b24f3968cf57bebc22c0868ce9a4ce7aab3ec8d
Mario TORBADO <mario.torbado@alten.es>
2019-08-21 13:17:20 +0200

    jenkinsfile new version

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,18 +1,23 @@
 pipeline {
     agent any
-
     stages {
         stage('Checkout') {
-        steps {
-            git url: 'https://github.com/zombivoro/Ejercicio2-WebTesting-Alten', branch: 'master'
+            steps {
+                git url: 'https://github.com/zombivoro/Ejercicio2-WebTesting-Alten', branch: 'master'
+            }
         }
-    }
-    stage('Test') {
-        steps {
-            sh 'chmod u+x src/test/resources/webdrivers/chrome/chromedriver.exe'
-            sh './gradlew clean build'
+        stage('Test') {
+            steps {
+                sh 'chmod u+x src/test/resources/webdrivers/chrome/chromedriver.exe'
+                sh 'chmod u+x src/test/resources/webdrivers/firefox/geckodriver.exe'
+
+                sh 'chmod u+x src/test/resources/webdrivers/chrome/chromedriver'
+                sh 'chmod u+x src/test/resources/webdrivers/firefox/geckodriver'
+
+                // browser is selected here, chrome or firefox tasks
+                sh './gradlew chrome'
+            }
         }
-    }
     }
     // post {
     //     always {
