4da510c570e6313a80e85c13ebe07790c93d1ff0
Mario TORBADO <mario.torbado@alten.es>
2019-08-21 13:33:58 +0200

    jenkinsfile with serenity report

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -19,16 +19,16 @@ pipeline {
             }
         }
     }
-    // post {
-    //     always {
-    //         publishHTML(target: [
-    //                 reportName : 'Serenity',
-    //                 reportDir:   'target/site/serenity',
-    //                 reportFiles: 'index.html',
-    //                 keepAll:     true,
-    //                 alwaysLinkToLastBuild: true,
-    //                 allowMissing: false
-    //         ])
-    //     }
-    // }
+     post {
+         always {
+             publishHTML(target: [
+                     reportName : 'Serenity',
+                     reportDir:   'target/site/serenity',
+                     reportFiles: 'index.html',
+                     keepAll:     true,
+                     alwaysLinkToLastBuild: true,
+                     allowMissing: false
+             ])
+         }
+     }
 }
