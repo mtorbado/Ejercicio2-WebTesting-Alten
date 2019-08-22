5173a9ef8beb82f2e341111ad428a1c95b617969
Mario TORBADO <mario.torbado@alten.es>
2019-08-22 10:59:05 +0200

    adding post always for pubishHTML plugin (serenity report)

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -14,9 +14,23 @@ pipeline {
                 sh 'chmod u+x src/test/resources/webdrivers/chrome/chromedriver'
                 sh 'chmod u+x src/test/resources/webdrivers/firefox/geckodriver'
 
-                // browser is selected here, chrome or firefox tasks
+                // browser is selected here, 'chrome' or 'firefox' gradle tasks
                 sh './gradlew chrome'
             }
+        }
+    }
+    post {
+        always {
+            publishHTML(
+                    target: [
+                            reportName :            'Serenity',
+                            reportDir:              'target/site/serenity',
+                            reportFiles:            'index.html',
+                            keepAll:                true,
+                            alwaysLinkToLastBuild:  true,
+                            allowMissing:           false
+                    ]
+            )
         }
     }
 }
