e5d6ef2dbb3d81f821db7065133dd4e3d82e9c68
Mario TORBADO <mario.torbado@alten.es>
2019-09-06 10:01:22 +0200

    jenkinsfile changes for new execution

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -15,9 +15,11 @@ pipeline {
                 sh 'chmod u+x src/test/resources/webdrivers/firefox/geckodriver'
 
                 // browser is selected here, 'chrome' or 'firefox' gradle tasks, or just 'test' to pass them via Jenkins parameters
-                sh "./gradlew clean test aggregate -Dwebdriver.remote.url=http://10.0.75.1:4444/wd/hub " +
-                        "-Dwebdriver.remote.driver=" + params.BROWSER + " -Dwebdriver.remote.os=WINDOWS" +
-                        " -Dserenity.driver.capabilities=applicationName:" + params.NODE
+//                sh "./gradlew clean test aggregate -Dwebdriver.remote.url=http://172.21.36.168:4444/wd/hub " +
+//                        "-Dwebdriver.remote.driver=" + params.BROWSER + " -Dwebdriver.remote.os=WINDOWS" +
+//                        " -Dserenity.driver.capabilities=applicationName:" + params.NODE
+
+                sh "./gradlew clean chrome aggregate -Dwebdriver.remote.url=http://172.21.36.168:4444/wd/hub "
             }
         }
     }
--- target/cucumber/report.js
+++ target/cucumber/report.js
@@ -95,8 +95,7 @@ formatter.match({
   "location": "RenfeSearchStepDefinitions.iWantToSearchTrainTickets()"
 });
 formatter.result({
-  "error_message": "java.lang.NullPointerException\r\n\tat com.page.RenfeSearchPageRemoteDriver.\u003cinit\u003e(RenfeSearchPageRemoteDriver.java:70)\r\n\tat com.serenitySteps.RenfeSearchSteps.openRenfeSearchPage(RenfeSearchSteps.java:22)\r\n\tat com.serenitySteps.RenfeSearchSteps$$EnhancerByCGLIB$$5c9032ae.CGLIB$openRenfeSearchPage$7(\u003cgenerated\u003e)\r\n\tat com.serenitySteps.RenfeSearchSteps$$EnhancerByCGLIB$$5c9032ae$$FastClassByCGLIB$$eeaaaed0.invoke(\u003cgenerated\u003e)\r\n\tat net.sf.cglib.proxy.MethodProxy.invokeSuper(MethodProxy.java:228)\r\n\tat net.thucydides.core.steps.BaseMethodRunner.invokeMethod(BaseMethodRunner.java:10)\r\n\tat net.thucydides.core.steps.NormalMethodRunner.invokeMethodAndNotifyFailures(NormalMethodRunner.java:20)\r\n\tat net.thucydides.core.steps.StepInterceptor.runNormalMethod(StepInterceptor.java:362)\r\n\tat net.thucydides.core.steps.StepInterceptor.testStepResult(StepInterceptor.java:133)\r\n\tat net.thucydides.core.steps.StepInterceptor.intercept(StepInterceptor.java:69)\r\n\tat com.serenitySteps.RenfeSearchSteps$$EnhancerByCGLIB$$5c9032ae.openRenfeSearchPage(\u003cgenerated\u003e)\r\n\tat com.stepDefinitions.RenfeSearchStepDefinitions.iWantToSearchTrainTickets(RenfeSearchStepDefinitions.java:22)\r\n\tat ✽.I want to search train tickets(src/test/resources/features/renfeSearch.feature:4)\r\n",
-  "status": "failed"
+  "status": "passed"
 });
 formatter.step({
   "name": "I select origin \"VALLADOLID\"",
@@ -106,7 +105,7 @@ formatter.match({
   "location": "RenfeSearchStepDefinitions.iSelectOrigin(String)"
 });
 formatter.result({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.step({
   "name": "I select destination \"MADRID-CHAMARTIN\"",
@@ -116,7 +115,7 @@ formatter.match({
   "location": "RenfeSearchStepDefinitions.iSelectDestination(String)"
 });
 formatter.result({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.step({
   "name": "departure date is 1 days after today",
@@ -126,7 +125,7 @@ formatter.match({
   "location": "RenfeSearchStepDefinitions.departureDateIsDaysAfterToday(int)"
 });
 formatter.result({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.step({
   "name": "return date is 2 days after departure date",
@@ -136,7 +135,7 @@ formatter.match({
   "location": "RenfeSearchStepDefinitions.comebackDateIsDaysAfterDepartureDate(int)"
 });
 formatter.result({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.step({
   "name": "I select the following adults \u0026 children \u0026 children under 4 years old",
@@ -162,7 +161,7 @@ formatter.match({
   "location": "RenfeSearchStepDefinitions.iSelectTheFolowingAdultsChildrenChildrenUnder4YearsOld(DataTable)"
 });
 formatter.result({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.step({
   "name": "I press \u0027comprar\u0027 button",
@@ -172,7 +171,7 @@ formatter.match({
   "location": "RenfeSearchStepDefinitions.iPressComprarButton()"
 });
 formatter.result({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.step({
   "name": "the result page is shown",
@@ -182,7 +181,7 @@ formatter.match({
   "location": "RenfeSearchStepDefinitions.theResultPageIsShown()"
 });
 formatter.result({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.scenario({
   "name": "Search for a trip",
@@ -197,8 +196,7 @@ formatter.match({
   "location": "RenfeSearchStepDefinitions.iWantToSearchTrainTickets()"
 });
 formatter.result({
-  "error_message": "java.lang.NullPointerException\r\n\tat com.page.RenfeSearchPageRemoteDriver.\u003cinit\u003e(RenfeSearchPageRemoteDriver.java:70)\r\n\tat com.serenitySteps.RenfeSearchSteps.openRenfeSearchPage(RenfeSearchSteps.java:22)\r\n\tat com.serenitySteps.RenfeSearchSteps$$EnhancerByCGLIB$$5c9032ae.CGLIB$openRenfeSearchPage$7(\u003cgenerated\u003e)\r\n\tat com.serenitySteps.RenfeSearchSteps$$EnhancerByCGLIB$$5c9032ae$$FastClassByCGLIB$$eeaaaed0.invoke(\u003cgenerated\u003e)\r\n\tat net.sf.cglib.proxy.MethodProxy.invokeSuper(MethodProxy.java:228)\r\n\tat net.thucydides.core.steps.BaseMethodRunner.invokeMethod(BaseMethodRunner.java:10)\r\n\tat net.thucydides.core.steps.NormalMethodRunner.invokeMethodAndNotifyFailures(NormalMethodRunner.java:20)\r\n\tat net.thucydides.core.steps.StepInterceptor.runNormalMethod(StepInterceptor.java:362)\r\n\tat net.thucydides.core.steps.StepInterceptor.testStepResult(StepInterceptor.java:133)\r\n\tat net.thucydides.core.steps.StepInterceptor.intercept(StepInterceptor.java:69)\r\n\tat com.serenitySteps.RenfeSearchSteps$$EnhancerByCGLIB$$5c9032ae.openRenfeSearchPage(\u003cgenerated\u003e)\r\n\tat com.stepDefinitions.RenfeSearchStepDefinitions.iWantToSearchTrainTickets(RenfeSearchStepDefinitions.java:22)\r\n\tat ✽.I want to search train tickets(src/test/resources/features/renfeSearch.feature:4)\r\n",
-  "status": "failed"
+  "status": "passed"
 });
 formatter.step({
   "name": "I select origin \"MADRID-PUERTA DE ATOCHA\"",
@@ -208,7 +206,8 @@ formatter.match({
   "location": "RenfeSearchStepDefinitions.iSelectOrigin(String)"
 });
 formatter.result({
-  "status": "skipped"
+  "error_message": "net.serenitybdd.core.exceptions.SerenityManagedException: The following error occurred: stale element reference: element is not attached to the page document\n  (Session info: chrome\u003d76.0.3809.87)\nFor documentation on this error, please visit: http://seleniumhq.org/exceptions/stale_element_reference.html\nBuild info: version: \u00273.8.1\u0027, revision: \u00276e95a6684b\u0027, time: \u00272017-12-01T18:33:54.468Z\u0027\nSystem info: host: \u0027SG-3012\u0027, ip: \u002710.0.75.1\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u002711.0.3\u0027\nDriver info: org.openqa.selenium.remote.RemoteWebDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 76.0.3809.87, chrome: {chromedriverVersion: 76.0.3809.68 (420c9498db8ce..., userDataDir: /tmp/.com.google.Chrome.YbiqHJ}, goog:chromeOptions: {debuggerAddress: localhost:38839}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: LINUX, platformName: LINUX, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify}\nSession ID: 7d29b4b6d882dfca536afa74ecd46634\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:490)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:164)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:601)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.execute(RemoteWebElement.java:279)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.getText(RemoteWebElement.java:160)\r\n\tat com.page.RenfeSearchPage.selectStation(RenfeSearchPage.java:93)\r\n\tat com.page.RenfeSearchPage.selectOrigin(RenfeSearchPage.java:82)\r\n\tat com.serenitySteps.RenfeSearchSteps.selectOrigin(RenfeSearchSteps.java:29)\r\n\tat com.serenitySteps.RenfeSearchSteps$$EnhancerByCGLIB$$5c9032ae.CGLIB$selectOrigin$5(\u003cgenerated\u003e)\r\n\tat com.serenitySteps.RenfeSearchSteps$$EnhancerByCGLIB$$5c9032ae$$FastClassByCGLIB$$eeaaaed0.invoke(\u003cgenerated\u003e)\r\n\tat net.sf.cglib.proxy.MethodProxy.invokeSuper(MethodProxy.java:228)\r\n\tat net.thucydides.core.steps.BaseMethodRunner.invokeMethod(BaseMethodRunner.java:10)\r\n\tat net.thucydides.core.steps.NormalMethodRunner.invokeMethodAndNotifyFailures(NormalMethodRunner.java:20)\r\n\tat net.thucydides.core.steps.StepInterceptor.runNormalMethod(StepInterceptor.java:362)\r\n\tat net.thucydides.core.steps.StepInterceptor.testStepResult(StepInterceptor.java:133)\r\n\tat net.thucydides.core.steps.StepInterceptor.intercept(StepInterceptor.java:69)\r\n\tat com.serenitySteps.RenfeSearchSteps$$EnhancerByCGLIB$$5c9032ae.selectOrigin(\u003cgenerated\u003e)\r\n\tat com.stepDefinitions.RenfeSearchStepDefinitions.iSelectOrigin(RenfeSearchStepDefinitions.java:27)\r\n\tat ✽.I select origin \"MADRID-PUERTA DE ATOCHA\"(src/test/resources/features/renfeSearch.feature:5)\r\n",
+  "status": "failed"
 });
 formatter.step({
   "name": "I select destination \"BARCELONA-SANTS\"",
